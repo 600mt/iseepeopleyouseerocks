79102454a6867fc6091478a470f0758e446f6b4e
600mt <teo,bettini@gmail.com>
2021-03-16 23:48:11 +0100

    style e mobile homepage

--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -1,32 +1,28 @@
 import React from 'react'
-import {motion} from 'framer-motion'
 
 import './resources/style.css'
 import Image_1 from './resources/img/1.png'
 import Image_2 from './resources/img/2.png'
 import Image_3 from './resources/img/3.png'
 import Image_4 from './resources/img/4.png'
-import {nimateSharedLayout, AnimatePresence}from 'framer-motion'
+import info_logo from './resources/img/navbarInfo.svg'
 
 export const Homepage = () => {
   return (
     <div>
       <div className="container-fluid">
         <div className="row" id='titleHomepage'> 
-          <div className="col-12 col-md-9">
-            <div id="r1">
+          <div className="col-sm-10 col-md-9" id="r1">
               <h2> I see <mark>  people</mark> you see <mark> rocks </mark> </h2>
-            </div>
           </div>
-        <div className="col-7 col-md-3">
-          <div id="r2">
-            <motion.h2> About </motion.h2>
-          </div>
+        <div className="col-sm-2 col-md-3" id="r2">
+             <h2 id="info_long"> <img src={info_logo} alt="info"></img> About </h2>
+             <img id="info_short" src={info_logo} alt="info"></img>
         </div>
       </div>
         <div className="row col-md-12" id="protocolli" style={{zIndex:10}}>
       <ul className="horizontal-slide">
-        <li className="child col-5 col-md-5">
+        <li className="child col-sm-2 col-md-5">
           <div id="card"> 
             <img alt='Not loaded'  className="image_dimension" src={Image_1} /> 
           </div>
@@ -35,7 +31,7 @@ export const Homepage = () => {
             <h4 id="r4"> A demo archive for an object-driven past exploration </h4>
           </div>
         </li>
-        <li className="child col-5 col-md-5">
+        <li className="child col-sm-2 col-md-5">
           <div 
             id="card"
            
@@ -47,7 +43,7 @@ export const Homepage = () => {
             <h4 id="r4"> Automatic image annotation and pictures that you will never see again </h4>
           </div>
         </li>
-        <li className="child col-5 col-md-5">
+        <li className="child col-sm-2 col-md-5">
           <div id="card"> 
             <img alt='Not loaded'  className="image_dimension" src={Image_3} /> 
           </div>
@@ -56,7 +52,7 @@ export const Homepage = () => {
             <h4 id="r4"> Photo memory books designed by iOS </h4>
           </div>
         </li>
-        <li className="child col-5 col-md-5">
+        <li className="child col-sm-2 col-md-5">
           <div id="card"> 
             <img alt='Not loaded'  className="image_dimension" src={Image_4} /> 
           </div>
