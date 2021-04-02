6bc80a526742fbd7a5927e26f515a2ce96ff5f57
600mt <teo,bettini@gmail.com>
2021-04-02 03:34:14 +0200

    modifiche

--- src/Pages/Protocol_2/FrameTwoPages.js
+++ src/Pages/Protocol_2/FrameTwoPages.js
@@ -10,19 +10,18 @@ import Image_6 from './resources/img/6.png'
 import Image_7 from './resources/img/7.png'
 import Image_8 from './resources/img/8.png'
 import Image_9 from './resources/img/9.png'
-import Image_10 from './resources/img/10.jpeg'
+import Image_10 from './resources/img/10.png'
 import Image_11 from './resources/img/11.png'
 import Image_12 from './resources/img/12.png'
 import Image_13 from './resources/img/13.png'
 import Image_14 from './resources/img/14.png'
 import Image_15 from './resources/img/15.png'
 import Image_16 from './resources/img/16.png'
-import Image_17 from './resources/img/20.png'
 import Navbar from './components/Navbar/Navbar'
 import ImageCarrousel from './components/ImageCarrousel/ImageCarrousel'
 
 export const FrameTwoPages = () => {
-  const datasetImages = [Image_1, Image_2, Image_3, Image_4, Image_5, Image_6, Image_7, Image_8, Image_9, Image_10, Image_11, Image_12, Image_13, Image_14, Image_15, Image_16, Image_17]
+  const datasetImages = [Image_1, Image_2, Image_3, Image_4, Image_5, Image_6, Image_7, Image_8, Image_9, Image_10, Image_11, Image_12, Image_13, Image_14, Image_15, Image_16]
   return (
     <div id='framTwoPages'>
         <Navbar />
