a5e6e7d10994c47fec761449a3344ce6ec84f025
Alexandru Enache <44054368+steinhardt21@users.noreply.github.com>
2021-04-18 20:22:32 +0200

    Update

--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -9,83 +9,130 @@ import Image_4 from './resources/img/4.png'
 import info_logo from './resources/img/navbarInfo.svg'
 
 import About  from './About'
+import { Fragment } from 'react'
 
 export const Homepage = () => {
   const [modalShow, setModalShow] = useState(false)
+  const [show, setShow] = useState(false)
   return (
-    <>
-     <div className="container-fluid" style={{ height: '8vh'}}>
-      <div className="row" id='titleHomepage'> 
-          <div className="col-10 col-md-9" id="r1">
-              <h2 className='fontTitoli'>I see people you see rocks </h2>
+    <Fragment style={{position: 'relative'}}>
+      <div style={{position: 'absolute', zIndex: 0}}>
+        <div className="container-fluid" 
+            style={{ 
+              height: '8vh',
+              marginTop:'2vh',
+              width: '100vw'
+            }}
+        >
+          <div className="row"> 
+              <div className="col-10 col-md-9" >
+                  <h2 className='fontTitoli'>I see people you see rocks </h2>
+              </div>
+              <div className="col-2 col-md-3">
+                <h2 style={{cursor: 'pointer', fontSize: '3.5vh'}}
+                  onClick={() => setShow(!show)}
+                  > <img src={info_logo} alt="info"></img> About </h2>
+              </div>
+              {/* <div className="col-2 col-md-3 d-md-none d-block" id="r2-short">
+                <img id="text-right" onClick={() => setModalShow(true)} src={info_logo} alt="info"></img>
+              </div> */}
           </div>
-          <div className="col-2 col-md-3" id="r2-long">
-             <h2 id="text-right" style={{cursor: 'pointer', fontSize: '3.5vh'}} onClick={() => setModalShow(true)}> <img src={info_logo} alt="info"></img> About </h2>
-             <About
-                  show={modalShow}
-                  clickMe={() => {}}
-                  onHide={() => setModalShow(false)}
-              />
-          </div>
-          <div className="col-2 col-md-3 d-md-none d-block" id="r2-short">
-             <img id="text-right" onClick={() => setModalShow(true)} src={info_logo} alt="info"></img>
-             <About
-                  show={modalShow}
-                  clickMe={() => {}}
-                  onHide={() => setModalShow(false)}
-              />
+        </div>
+          <div className="container-fluid">
+            <div className="row col-md-12" id="protocolli" style={{zIndex:10}}>
+              <ul className="horizontal-slide">
+                <li className="child col-sm-2 col-md-5">
+                  <div id="card"> 
+                  <Link to='/protocollo-1'> <img alt='Not loaded'  className="image_dimension" src={Image_1} /> </Link>
+                  </div>
+                  <div> 
+                    <h3 id="r3"> Algorithms are explorers </h3> 
+                    <h4 id="r4" className="text-wrap"> A demo archive for an object-driven past exploration </h4>
+                  </div>
+                </li>
+                <li className="child col-sm-2 col-md-5">
+                  <div 
+                    id="card"
+                  
+                  > 
+                    <Link to='/protocollo-2'><img alt='Not loaded'  className="image_dimension" src={Image_2} /> </Link>
+                  </div>
+                  <div> 
+                    <h3 id="r3"> Algorithms are forgetful </h3>
+                    <h4 id="r4" className="text-wrap"> Automatic image annotation and pictures that you will never see again </h4>
+                  </div>
+                </li>
+                <li className="child col-sm-2 col-md-5">
+                  <div id="card"> 
+                    <img alt='Not loaded'  className="image_dimension" src={Image_3} /> 
+                  </div>
+                  <div> 
+                    <h3 id="r3"> Algorithms are curators </h3>
+                    <h4 id="r4" className="text-wrap"> Photo memory books designed by iOS </h4>
+                  </div>
+                </li>
+                <li className="child col-sm-2 col-md-5">
+                  <div id="card"> 
+                    <img alt='Not loaded'  className="image_dimension" src={Image_4} /> 
+                  </div>
+                  <h3 id="r3"> Algorithms are keen to adventures </h3>
+                  <h4 id="r4" className="text-wrap" > About why one third of my personal pictures are recognized as adventure, expedition and holidays and what they look like.</h4>
+                </li>
+              </ul>
+            </div>
+          <div className="row">
+            <div className="col-12">
+                <About />
+            </div>
           </div>
         </div>
       </div>
-      <div className="container-fluid">
-        <div className="row col-md-12" id="protocolli" style={{zIndex:10}}>
-      <ul className="horizontal-slide">
-        <li className="child col-sm-2 col-md-5">
-          <div id="card"> 
-          <Link to='/protocollo-1'> <img alt='Not loaded'  className="image_dimension" src={Image_1} /> </Link>
+
+
+     
+
+    <div id='aboutProtocollo1' style={{position: 'absolute', zIndex: 2, display: (show ? 'block' : 'none')}} >
+      {/* backdrop-filter: blur(10px); */}
+        {/* <div style={{position: 'absolute',  filter: 'blur(20px)', height: '300px', zIndex: 1, width: '300px', backgroundColor:  'white'}} /> */}
+      <div style={{position: 'absolute', height: '100%', width: '100%'}}>
+        <div className="container-fluid" 
+          style={{
+            position: 'relative',
+            top: '1vh',
+            backgroundColor: 'yellow', 
+            height: '98vh', 
+            width: '98vw', 
+            // paddingTop: '2%',
+            // marginTop: '2%',
+            borderRadius: '20px'
+          }}
+        >
+          <div className="row"> 
+            <div className="col-2 col-md-3">
+              {/* <Link to='/'> 
+                <img style={{cursor: 'pointer', width:'1.5vh', marginTop: '3.6%', marginLeft: '6%'}} src={back_logo} alt="info" />
+              </Link> */}
+            </div>
+            <div className="col-8 col-md-6 text-center">
+                <h2 className='fontTitoli' style={{paddingLeft: '3%',  marginTop: '1.2%'}}>Algorithms are explorers</h2>
+            </div>
+            <div className="col-2 col-md-3">
+              {/* <img onClick={() => setShow(!show)} style={{cursor: 'pointer', width: '3.5vh', marginTop: '2.6%', marginLeft: '86.5%'}} src={info_logo} alt="info" /> */}
+            </div>
           </div>
-          <div> 
-            <h3 id="r3"> Algorithms are explorers </h3> 
-            <h4 id="r4" className="text-wrap"> A demo archive for an object-driven past exploration </h4>
+          <div className='row'>
+            <div className='col-10 offset-1'>
+              <p>
+                Object-recognition technologies, deployed via machine learning, analyse contents and label hundreds of pictures in a few moments every day on every iPhone. These technologies enable object-level categorisation and an easier image search by keyword. <br /><br />
+                By comparing human vision with what is recognized by the on-device feature extraction, the artifact displays what was labelled correctly and leaves few clues of what it was not. The limits of interpretation by the machine in object recognition mimics and amplifies the very human feature of forgetting the past. What is recognized by the algorithm is going to be saved for future research, making it browsable again in my gallery. On the other hand, the non-recognized objects cannot be found and could cause the whole picture to get lost in the huge amount of images we produce. <br /><br />
+                In this way multiple recognized fragments of past overlap and merge, becoming new pictures of what it has been. Losing details and contexts of single moments, they finally reach a new status: a continuous stream of partial memories.  
+              </p>
+            </div>
           </div>
-        </li>
-        <li className="child col-sm-2 col-md-5">
-          <div 
-            id="card"
-           
-          > 
-            <Link to='/protocollo-2'><img alt='Not loaded'  className="image_dimension" src={Image_2} /> </Link>
-          </div>
-          <div> 
-            <h3 id="r3"> Algorithms are forgetful </h3>
-            <h4 id="r4" className="text-wrap"> Automatic image annotation and pictures that you will never see again </h4>
-          </div>
-        </li>
-        <li className="child col-sm-2 col-md-5">
-          <div id="card"> 
-            <img alt='Not loaded'  className="image_dimension" src={Image_3} /> 
-          </div>
-          <div> 
-            <h3 id="r3"> Algorithms are curators </h3>
-            <h4 id="r4" className="text-wrap"> Photo memory books designed by iOS </h4>
-          </div>
-        </li>
-        <li className="child col-sm-2 col-md-5">
-          <div id="card"> 
-            <img alt='Not loaded'  className="image_dimension" src={Image_4} /> 
-          </div>
-          <h3 id="r3"> Algorithms are keen to adventures </h3>
-          <h4 id="r4" className="text-wrap" > About why one third of my personal pictures are recognized as adventure, expedition and holidays and what they look like.</h4>
-        </li>
-      </ul>
-    </div>
-    <div className="row">
-     <div className="col-12">
-        <About />
-     </div>
-    </div>
+        </div>   
       </div>
-   </>
+    </div>
+   </Fragment>
 )}
 
 export default Homepage
--- src/Pages/Protocol_2/FrameTwoPages.js
+++ src/Pages/Protocol_2/FrameTwoPages.js
@@ -42,23 +42,39 @@ export const FrameTwoPages = () => {
   
   return (
   <Fragment style={{position: 'relative'}}>
-    
-    <div className="container-fluid" style={{zIndex: 0, backgroundColor: 'white', height: '8vh'}}>
-      <div className="row" id='titleHomepage'> 
-          <div className="col-2 col-md-3" id="r3-long-back">
-          <Link to='/'> <h2 id="text-right" style={{cursor: 'pointer'}}><img id='iconBack' style={{cursor: 'pointer'}} src={back_logo} alt="info"></img></h2></Link>
+    <div style={{position: 'absolute', zIndex: 0}}>
+      <div className="container-fluid" style={{
+        backgroundColor: 'white', 
+        // position: 'relative',
+        // top: '1%', 
+        // paddingTop: '2%',
+        // height: '99%', width: '99%'
+          height: '7vh',
+          marginTop:'1vh', 
+          width: '98vw',
+        }}
+      >
+        <div className="row"> 
+          <div className="col-2 col-md-3">
+            <Link to='/'> 
+              <img style={{cursor: 'pointer', width:'1.5vh', marginTop: '3.6%', marginLeft: '6%'}} src={back_logo} alt="info" />
+            </Link>
           </div>
-          <div className="col-8 col-md-6 text-center" id="r1-protocol2">
-              <h2 className='fontTitoli'>Algorithms are explorers</h2>
+          <div className="col-8 col-md-6 text-center">
+              <h2 className='fontTitoli' style={{paddingLeft: '3%',  marginTop: '1.2%'}}>Algorithms are explorers</h2>
           </div>
-          <div className="col-2 col-md-3" id="r3-long">
-             <h2 id="text-right" style={{cursor: 'pointer'}}><img onClick={() => setShow(!show)} id='iconLeft' style={{cursor: 'pointer'}} src={info_logo} alt="info"></img></h2>
+          <div className="col-2 col-md-3">
+            <img onClick={() => setShow(!show)} style={{cursor: 'pointer', width: '3.5vh', marginTop: '2.6%', marginLeft: '86.5%'}} src={info_logo} alt="info" />
           </div>
+        </div>
       </div>
-    </div>
 
-    <div id='framTwoPages'  style={{width: '100vw', position: 'absolute', zIndex: 0}}>
-        {/* <Navbar setShow={setShow} show={show} /> */}
+      <div id='framTwoPages'  
+        style={{
+          width: '100vw',
+          height: '89vh'
+        }}
+      >
         <div id='firstTopImages' />
         <div id='imageAndMaskProtocollo2'>
           <div id='imageBackground'>
@@ -73,64 +89,48 @@ export const FrameTwoPages = () => {
           </div>
         </div>
       </div>
-
+    </div>
     <div id='aboutProtocollo1' style={{position: 'absolute', zIndex: 2, display: (show ? 'block' : 'none')}} >
       {/* backdrop-filter: blur(10px); */}
-      <div style={{position: 'relative'}}>
-        <div style={{position: 'absolute',  filter: 'blur(20px)', height: '300px', zIndex: 1, width: '300px', backgroundColor:  'white'}} />
-        <div style={{position: 'absolute', height: '100%', width: '100%'}}>
-
-          <div id='titleHomepageAbout'> 
-            <div className="col-2 col-md-3" id="r3-long-back" style={{paddingLeft: '1%', paddingTop: '1vh'}}>
-            <Link to='/'> <h2 id="text-right" style={{cursor: 'pointer'}}><img id='iconBack' style={{cursor: 'pointer'}} src={back_logo} alt="info"></img></h2></Link>
+        {/* <div style={{position: 'absolute',  filter: 'blur(20px)', height: '300px', zIndex: 1, width: '300px', backgroundColor:  'white'}} /> */}
+      <div style={{position: 'absolute', height: '100%', width: '100%'}}>
+        <div className="container-fluid" 
+          style={{
+            position: 'relative',
+            top: '1vh',
+            backgroundColor: 'yellow', 
+            height: '98vh', 
+            width: '98vw', 
+            // paddingTop: '2%',
+            // marginTop: '2%',
+            borderRadius: '20px'
+          }}
+        >
+          <div className="row"> 
+            <div className="col-2 col-md-3">
+              <Link to='/'> 
+                <img style={{cursor: 'pointer', width:'1.5vh', marginTop: '3.6%', marginLeft: '6%'}} src={back_logo} alt="info" />
+              </Link>
             </div>
-            <div className="col-8 col-md-6 text-center" id="r1-protocol2" style={{paddingTop: '1vh'}}>
-                <h2 className='fontTitoli'>Algorithms are explorers</h2>
+            <div className="col-8 col-md-6 text-center">
+                <h2 className='fontTitoli' style={{paddingLeft: '3%',  marginTop: '1.2%'}}>Algorithms are explorers</h2>
             </div>
-            <div className="col-2 col-md-3" id="r3-long" style={{paddingRight: '1%', paddingTop: '1vh'}}>
-              <h2 id="text-right" style={{cursor: 'pointer'}}><img onClick={() => setShow(!show)} id='iconLeft' style={{cursor: 'pointer'}} src={info_logo} alt="info"></img></h2>
+            <div className="col-2 col-md-3">
+              <img onClick={() => setShow(!show)} style={{cursor: 'pointer', width: '3.5vh', marginTop: '2.6%', marginLeft: '86.5%'}} src={info_logo} alt="info" />
             </div>
           </div>
-
-          <p>
-            Object-recognition technologies, deployed via machine learning, analyse contents and label hundreds of pictures in a few moments every day on every iPhone. These technologies enable object-level categorisation and an easier image search by keyword. <br /><br />
-            By comparing human vision with what is recognized by the on-device feature extraction, the artifact displays what was labelled correctly and leaves few clues of what it was not. The limits of interpretation by the machine in object recognition mimics and amplifies the very human feature of forgetting the past. What is recognized by the algorithm is going to be saved for future research, making it browsable again in my gallery. On the other hand, the non-recognized objects cannot be found and could cause the whole picture to get lost in the huge amount of images we produce. <br /><br />
-            In this way multiple recognized fragments of past overlap and merge, becoming new pictures of what it has been. Losing details and contexts of single moments, they finally reach a new status: a continuous stream of partial memories.  
-          </p>
-        </div>
-
-      </div>
-        
-        {/* <div id='framTwoPagesAbout'>
-            <NavbarAbout setShow={setShow} show={show} />
-            <div id='firstTopImages' />
-            <div id='imageAndMask'>
-            <div id='imageBackground'>
-                <div id='firstLeftBorderImage' />
-                <div id='secondMaskImageRealAbout' style={{fontSize: '110%'}}>
+          <div className='row'>
+            <div className='col-10 offset-1'>
+              <p>
                 Object-recognition technologies, deployed via machine learning, analyse contents and label hundreds of pictures in a few moments every day on every iPhone. These technologies enable object-level categorisation and an easier image search by keyword. <br /><br />
                 By comparing human vision with what is recognized by the on-device feature extraction, the artifact displays what was labelled correctly and leaves few clues of what it was not. The limits of interpretation by the machine in object recognition mimics and amplifies the very human feature of forgetting the past. What is recognized by the algorithm is going to be saved for future research, making it browsable again in my gallery. On the other hand, the non-recognized objects cannot be found and could cause the whole picture to get lost in the huge amount of images we produce. <br /><br />
-                In this way multiple recognized fragments of past overlap and merge, becoming new pictures of what it has been. Losing details and contexts of single moments, they finally reach a new status: a continuous stream of partial memories.
-                </div>
-                <div id='thirdCentralImage' />
-                <div id='fourthMaskImageRealAbout'>
-                </div>
-                <div id='fifthRightBorderImage' />
+                In this way multiple recognized fragments of past overlap and merge, becoming new pictures of what it has been. Losing details and contexts of single moments, they finally reach a new status: a continuous stream of partial memories.  
+              </p>
             </div>
-            <div id='secondMask'>
-                <div id='firstPartMask'>
-                <div id='firstLeftBorderAbout' />
-                <div id='secondMaskImage' />
-                <div id='thirdCentralAbout' />
-                <div id='fourthMaskImage' />
-                <div id='fifthRightBorderAbout' />
-                </div>
-                <div id='secondPartMaskAbout'>
-                </div>
-            </div>
-            </div>
-        </div> */}
+          </div>
+        </div>   
       </div>
+    </div>
   </Fragment>
   )
 }
