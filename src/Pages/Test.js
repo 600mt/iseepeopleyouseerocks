d9779747c254afce6d2a0fc9487ebfdbc0fc3a5e
Alexandru Enache <44054368+steinhardt21@users.noreply.github.com>
2021-04-24 20:40:16 +0200

    Update

--- src/Pages/Test.js
+++ src/Pages/Test.js
@@ -1,4 +1,16 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {Link} from 'react-router-dom'
+
+import './resources/style.css'
+import Image_1 from './resources/img/1.png'
+import Image_2 from './resources/img/2.png'
+import Image_3 from './resources/img/3.png'
+import Image_4 from './resources/img/4.png'
+import info_logo from './resources/img/navbarInfo.svg'
+import close_logo from './resources/img/close.svg'
+
+import About  from './About'
+import { Fragment } from 'react'
 
 export const Test = () => {
     return (
