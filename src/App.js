0e5bed43ab98249cdd68f9097aefe0340a5a0eac
Alexandru Enache <44054368+steinhardt21@users.noreply.github.com>
2021-04-24 20:32:58 +0200

    update

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <Test />
+        {/* <Test /> */}
         <a
           className="App-link"
           href="https://reactjs.org"
